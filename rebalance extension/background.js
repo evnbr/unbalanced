fa4567b5b78550ae3bdd83e19e56e3a41fcd5f74
Evan Brooks <brooks.ev@gmail.com>
2013-12-03 21:46:04 -0500

    initial

---
+++ rebalance extension/background.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+
+  chrome.tabs.executeScript(null, {
+    file: 'handlebars-v1.1.2.js'
+  });
+
+  chrome.tabs.executeScript(null, {
+    file: 'template.js'
+  });
+
+  chrome.tabs.executeScript(null, {
+    file: 'script.js'
+  });
+
+});
---
+++ testing/script.js
@@ -0,0 +1,103 @@
+var goog_script = document.createElement('script');
+goog_script.setAttribute('src','https://www.google.com/jsapi');
+document.head.appendChild(goog_script);
+
+var feed_url = {
+  news: "http://america.aljazeera.com/content/ajam/articles.rss",
+  // news: "http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml",
+  video: "http://www.nytimes.com/video/opinion/?rss=1&pagewanted=all",
+}
+
+var feed_data = {};
+
+
+
+google.load("feeds", "1");
+google.setOnLoadCallback(onload_makefeeds);
+
+function onload_makefeeds() {
+
+  news = new google.feeds.Feed(feed_url.news);
+  news.includeHistoricalEntries();
+  news.setNumEntries(30);
+  news.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
+
+
+  vids = new google.feeds.Feed(feed_url.video);
+  vids.includeHistoricalEntries();
+  vids.setNumEntries(30);
+  vids.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
+
+
+  // var query = 'site:cnn.com president';
+  // google.feeds.findFeeds(query, findDone);
+
+  news.load(news_loaded);
+  vids.load(vids_loaded);
+}
+
+function news_loaded(result) {
+  if (!result.error) {
+    // console.log(result.xmlDocument);
+    // console.log(result.feed.entries);
+
+    var processed_feed = result.feed.entries.map(function(el){
+      el.image = $(el.xmlNode).find("enclosure").attr("url");
+      return el;
+    });
+
+    feed_data.news = processed_feed;
+    async_done();
+  }
+}
+
+function vids_loaded(result) {
+  if (!result.error) {
+    // console.log(result.xmlDocument);
+    // console.log(result.feed.entries);
+
+    var processed_feed = result.feed.entries.map(function(el){
+      el.image = $(el.xmlNode).find("[url *= jpg]").attr("url");
+      return el;
+    });
+
+    feed_data.vids = processed_feed;
+    async_done();
+  }
+}
+
+function async_done() {
+  if ( feed_data.news && feed_data.vids ) {
+    render_context();
+  }
+}
+
+function render_context() {
+  var context = {
+    main: {
+      article: feed_data.news[0],
+      related: []
+    }, 
+    secondary: [
+      {
+        article: feed_data.news[1],
+        related: []
+      },
+      {
+        article: feed_data.news[2],
+        related: []
+      },
+      {
+        article: feed_data.news[3],
+        related: []
+      },
+    ],
+    latest: {
+      left: feed_data.news.slice(4, 14),
+      right: feed_data.news.slice(14, 24),
+    },
+    videos: feed_data.vids.slice(0, 6),
+  }
+  var html = Handlebars.templates.fox(context);
+  $("#section").html(html);
+}
